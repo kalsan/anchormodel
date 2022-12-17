c0eabfeff8454279d38e9935e77f989baed2b040
Sandro Kalbermatter <kalsan@users.noreply.github.com>
2022-12-17 11:26:26 +0100

    Implement casting

--- lib/anchormodel/active_model_type_value.rb
+++ lib/anchormodel/active_model_type_value.rb
@@ -2,4 +2,20 @@ class Anchormodel::ActiveModelTypeValue < ActiveModel::Type::Value
   def initialize(attribute)
     @attribute = attribute
   end
+
+  def cast(value)
+    return case value
+    when Symbol, String
+      unless @anchor_association.anchor_class.valid_keys.include?(value.to_sym)
+        fail("Attempt to set #{@anchor_association.attribute_name} to unsupported key #{value.inspect}.")
+      end
+      value.to_s
+    when @anchor_association.anchor_class
+      value.key.to_s
+    when nil
+      nil
+    else
+      fail "Attempt to set #{@anchor_association.attribute_name} to unsupported type #{value.class}"
+    end
+  end
 end
