315653256b2e308b13d875fd670d52a23b9e3f88
Sandro Kalbermatter <kalsan@users.noreply.github.com>
2022-12-17 10:56:32 +0100

    Create basic class Anchormodel

--- lib/anchormodel.rb
+++ lib/anchormodel.rb
@@ -1,4 +1,23 @@
 class Anchormodel
+  attr_reader :key
+  attr_reader :index
+  class_attribute :entries_list, default: [] # For ordering
+  class_attribute :entries_hash, default: {} # For quick access
+
+  # To set @foo=:bar for anchor :ter, use new(:ter, foo: :bar)
+  def initialize(key, **attributes)
+    @key = key.to_sym
+    @index = self.entries_list.count
+
+    # Save attributes as instance variables
+    attributes.each do |k, v|
+      instance_variable_set(:"@#{k}", v)
+    end
+
+    # Register self
+    entries_list << self
+    entries_hash[key] = self
+  end
 end
 
 require 'anchormodel/version'
