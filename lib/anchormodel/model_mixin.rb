c3b91abcc85466cbdc6f815423916bbb45e3bfc1
Sandro Kalbermatter <kalsan@users.noreply.github.com>
2022-12-17 11:24:29 +0100

    Implement a basic belongs_to_anchormodel attribute helper

--- lib/anchormodel/model_mixin.rb
+++ lib/anchormodel/model_mixin.rb
@@ -5,4 +5,25 @@ module Anchormodel::ModelMixin
     class_attribute :anchormodel_attributes, default: {}.freeze
   end
 
+  class_methods do
+    def belongs_to_anchormodel(attribute_name, anchor_class_name=nil, optional: false)
+      attribute_name = attribute_name.to_sym
+      attribute = Anchormodel::Attribute.new(self, attribute_name, anchor_class_name, optional)
+
+      # Register attribute
+      self.anchormodel_attributes = anchormodel_attributes.merge({attribute_name => attribute}).freeze
+
+      # Add presence validation if required
+      unless optional
+        validates attribute_name, presence: true
+      end
+
+      # Make casting work
+      # Define serializer/deserializer
+      active_model_type_value = Anchormodel::ActiveModelTypeValue.new(attribute)
+
+      # Supply serializer and deserializer
+      attribute attribute_name, active_model_type_value
+    end
+  end
 end
