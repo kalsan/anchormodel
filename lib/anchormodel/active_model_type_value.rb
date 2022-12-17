c6700986f2536ad6e37374c28c9529e2cf20946d
Sandro Kalbermatter <kalsan@users.noreply.github.com>
2022-12-17 11:29:01 +0100

    Move cast code to serialize because cast does not appear to be enough

--- lib/anchormodel/active_model_type_value.rb
+++ lib/anchormodel/active_model_type_value.rb
@@ -4,18 +4,23 @@ class Anchormodel::ActiveModelTypeValue < ActiveModel::Type::Value
   end
 
   def cast(value)
+    serialize value
+  end
+
+  # Implementing this instead of cast to force key validation in any case
+  def serialize(value)
     return case value
     when Symbol, String
-      unless @anchor_association.anchor_class.valid_keys.include?(value.to_sym)
-        fail("Attempt to set #{@anchor_association.attribute_name} to unsupported key #{value.inspect}.")
+      unless @attribute.anchor_class.valid_keys.include?(value.to_sym)
+        fail("Attempt to set #{@attribute.attribute_name} to unsupported key #{value.inspect}.")
       end
       value.to_s
-    when @anchor_association.anchor_class
+    when @attribute.anchor_class
       value.key.to_s
     when nil
       nil
     else
-      fail "Attempt to set #{@anchor_association.attribute_name} to unsupported type #{value.class}"
+      fail "Attempt to set #{@attribute.attribute_name} to unsupported type #{value.class}"
     end
   end
 end
