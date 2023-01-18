4dbe35b1d9a3e5c38187af8df490560482e07312
Sandro Kalbermatter <kalsan@users.noreply.github.com>
2023-01-18 11:13:58 +0100

    Remove unused bundle script and regenerate schema

--- test/dummy/db/schema.rb
+++ test/dummy/db/schema.rb
@@ -10,11 +10,12 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 20_230_107_173_151) do
-  create_table 'users', force: :cascade do |t|
-    t.string 'role'
-    t.string 'locale'
-    t.datetime 'created_at', null: false
-    t.datetime 'updated_at', null: false
+ActiveRecord::Schema[7.0].define(version: 2023_01_07_173151) do
+  create_table "users", force: :cascade do |t|
+    t.string "role"
+    t.string "locale"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
   end
+
 end
