616e25bb9c018e3965fcc469524c2e437264e9b2
Sandro Kalbermatter <kalsan@users.noreply.github.com>
2023-05-19 12:06:52 +0200

    Switch back to edge for future versions

--- lib/anchormodel/version.rb
+++ lib/anchormodel/version.rb
@@ -2,9 +2,9 @@ class Anchormodel
   module Version
     MAJOR = 0
     MINOR = 1
-    PATCH = 3
+    PATCH = 4
 
-    EDGE = false
+    EDGE = true
 
     LABEL = [MAJOR, MINOR, PATCH, EDGE ? 'edge' : nil].compact.join('.')
   end
